7ce196d82edffb352adc35cff149756b814c7034
gautamgitspace <agautam2@buffalo.edu>
2020-04-02 23:18:46 +0530

    #0 : alternatives A and B

--- 0_single_number.py
+++ 0_single_number.py
@@ -8,4 +8,31 @@ class Solution(object):
         for num in nums:
             res ^= num
         return res
-        
+
+    def singleNumberAltA(self, nums):
+        """
+        using dict
+        """
+        d = {}
+        for num in nums:
+            if num not in d:
+                d[num] = 1
+            else:
+                d[num] += 1
+        for k, v in d.items():
+            if v == 1:
+                return k
+
+    def singleNumberAltB(self, nums):
+        """
+        using set
+        """
+        collect = set()
+        for num in nums:
+            if num not in collect:
+                collect.add(num)
+            else:
+                collect.remove(num)
+
+        for item in collect:
+            return item
