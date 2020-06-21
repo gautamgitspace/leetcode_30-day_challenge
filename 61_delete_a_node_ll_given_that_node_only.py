d326d7a8ab2d2391e3ae8ba00ebbba44c781ada5
gautamgitspace <agautam2@buffalo.edu>
2020-06-21 03:03:04 -0700

    #79 : permute seq, check if upststeam branh is NOT gone

--- 61_delete_a_node_ll_given_that_node_only.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def deleteNode(self, node):
-        """
-        Q : delete a node in the LL given access to that node only
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
