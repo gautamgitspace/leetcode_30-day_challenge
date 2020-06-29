424be53a64831a47c04d5a52af20b453e3429f48
gautamgitspace <agautam2@buffalo.edu>
2020-06-28 21:26:51 -0700

    #87 : stitching IB and OB airports, dict and stacks

---
+++ 87_reconstruct_itinerary.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        graph = defaultdict(list)
+        for s, d in tickets:
+            graph[s].append(d)
+
+        # sort destinations for each source
+        for source in graph:
+            graph[source].sort(reverse = True)
+
+        stack, res = ['JFK'], []
+
+        # keep checking if TOS exists in our dict
+        # if it does, pop the value for this key
+        # and append back to stack, this way we
+        # stitch SOURCE and DEST. When we don't
+        # have a hit in the dict, we pop from
+        # stack and append to res
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop())
+            res.append(stack.pop())
+        return res[::-1]
